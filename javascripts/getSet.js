1c36ec92f86c28797e263f1f84395b9be71cd9d6
Benjamin Wilburn <bwilburnstrength@gmail.com>
2016-05-18 15:42:58 -0500

    update gh-pages

---
+++ javascripts/getSet.js
@@ -0,0 +1,71 @@
+'use strict';
+var Chatty = (function(aug) {
+
+	var messages = [];
+	var idCounter = 0;
+
+	var outputDiv = $("#msgArea");
+
+	function Message (string, handle, person, updateTime) {
+		this.string = string;
+		this.handle = handle;
+		this.user = person;
+		this.time = updateTime;
+	}
+
+	aug.addNewMessage = function (newItem, person) {
+		var updateTime = new Date();
+		updateTime = updateTime.toLocaleTimeString() + " " + updateTime.toLocaleDateString();
+		var newMsg = new Message(newItem, `msg${idCounter}`, person, updateTime);
+		messages.push(newMsg);
+		idCounter++;
+		Chatty.loadMessages();
+	};
+
+	aug.editMessage = function (editText, editId) {
+		var updateTime = new Date();
+		updateTime = updateTime.toLocaleTimeString() + " " + updateTime.toLocaleDateString();
+		var toEdit = messages.find(function(message) {
+			return message.handle === editId;
+		});
+		console.log("trouble", editId);
+		// toEdit.string = editText;
+		Chatty.loadMessages();
+	};
+
+	aug.getMessages = function () {
+		return messages;
+	};
+
+	aug.deleteData = function (ex) {
+		messages = messages.filter(function(message) {
+			return message.handle !== ex;
+		});
+		Chatty.loadMessages();
+	};
+
+	aug.loadMessages = function () {
+		var buildHTML = "";
+		alert("4");
+		if (messages.length >= 20){
+			messages.shift(messages[0]);
+		}
+		for (var i = (messages.length - 1); i >= 0; i--) {
+			messages[i].handle = `msg${i}`; //WHEN YOU DO A DELETE NEED TO RESET HANDLES
+
+			console.log("works2");
+			buildHTML += `<p id="${messages[i].handle}" class="message strong">${messages[i].user}: <label class='userMsg'>${messages[i].string} </label><button class="edit">Edit</button><button class="delete">Delete</button><span class="timeStamp">${messages[i].time}</span></p>`;
+		}
+		outputDiv.html(buildHTML);
+		$(".delete").each(function() {
+			$(this).click(Chatty.deleteMsg);
+		});
+		$(".edit").each(function(){
+			$(this).click(editMsg);
+		});
+	}
+
+
+	return aug;
+
+}(Chatty || {}));
---
+++ javascripts/xhr.js
@@ -0,0 +1,30 @@
+'use strict';
+var Chatty= (function(xhr){
+
+	var jsonMessages= [];
+
+	$.ajax({
+    url:"messages.json"
+  }).done(fetchMsg).fail(function (jqXHR, textStatus){
+  	alert(textStatus);
+  });
+
+
+	function fetchMsg (jsonContents) {
+		for (var i = 0; i < jsonContents.messages.length; i++) {
+			jsonMessages.push(jsonContents.messages);
+			Chatty.addNewMessage(jsonContents.messages[i].message,jsonContents.messages[i].user);
+			console.log(jsonContents.messages);
+		}
+	}
+
+	xhr.getJson= function () {
+		return jsonMessages;
+	};
+
+	return xhr;
+
+}(Chatty || {}));
+
+
+
